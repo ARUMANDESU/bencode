3fda0e70538401b479a846f6299f40ac4647a144
ARUMANDESU <zh.arumandes@gmail.com>
2026-09-14 06:40:34 +0500

    test(decode): add benchmark for real torrent file

--- decode_bench_test.go
+++ decode_bench_test.go
@@ -1,10 +1,14 @@
 package bencode
 
 import (
+	"bytes"
 	"fmt"
+	"os"
+	"path/filepath"
 	"testing"
 
 	bencodeast "github.com/arumandesu/bencode/pkg/bencode_ast"
+	"github.com/stretchr/testify/require"
 )
 
 // repeatedString returns a bencoded string of exactly n bytes of payload.
@@ -130,4 +134,40 @@ func BenchmarkMapDestination(b *testing.B) {
 			mustUnmarshal(b, in, &got)
 		}
 	})
+}
+
+func benchTorrent(b *testing.B) []byte {
+	b.Helper()
+	matches, err := filepath.Glob(filepath.Join("testdata", "*.torrent"))
+	if err != nil || len(matches) == 0 {
+		b.Skip("no .torrent fixture in testdata")
+	}
+	in, err := os.ReadFile(matches[0])
+	if err != nil {
+		b.Fatal(err)
+	}
+	return in
+}
+
+func BenchmarkUnmarshalTorrent(b *testing.B) {
+	in := benchTorrent(b)
+	b.SetBytes(int64(len(in)))
+	b.ReportAllocs()
+	for b.Loop() {
+		var got torrentMeta
+		if err := Unmarshal(in, &got); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkDecoderTorrent(b *testing.B) {
+	in := benchTorrent(b)
+	b.SetBytes(int64(len(in)))
+	b.ReportAllocs()
+	for b.Loop() {
+		var got torrentMeta
+		err := NewDecoder(bytes.NewReader(in)).Decode(&got)
+		require.NoError(b, err)
+	}
 }
